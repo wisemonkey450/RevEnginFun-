bc67ad0ba5abdd01653e569c3ac098f2bc715fd9
root <root@localhost.localdomain>
2019-02-14 05:31:18 -0500

    headers

--- obfuscation_xor.py
+++ obfuscation_xor.py
@@ -2,46 +2,54 @@ import binascii as bs
 import os
 import sys
 
+#Generates a header file that can be included into the
+#C code and will contain the strings
 
-if len(sys.argv) < 2:
-    print "You need to give me something dude!"
-    exit(-1)
-#CHANGE THIS
-var = sys.argv[1]
-#RANDO KEY
-key = os.urandom(len(var))
+header_name = "obfuscate_hdr.h"
 
-enc = [ chr(ord(a) ^ ord(b)) for (a,b) in zip(var, key) ]
-dec = [ chr(ord(a) ^ ord(b)) for (a,b) in zip(enc, key) ]
-pkey = [ chr(ord(a) ^ ord(b)) for (a,b) in zip(enc, dec) ]
+fd = open(header_name, 'a+')
+
+print "Input a string to be XORed >> "
+string = raw_input()
+
+while "Quit" not in string:
+    #CHANGE THIS
+    var = string
+    #RANDO KEY
+    key = os.urandom(len(var))
+
+    enc = [ chr(ord(a) ^ ord(b)) for (a,b) in zip(var, key) ]
+    dec = [ chr(ord(a) ^ ord(b)) for (a,b) in zip(enc, key) ]
+    pkey = [ chr(ord(a) ^ ord(b)) for (a,b) in zip(enc, dec) ]
 
 
-print "You can insert these value as byte array for C. Please make sure to use the proper syntax or bad things will happen."
+    print "You can insert these value as byte array for C. Please make sure to use the proper syntax or bad things will happen."
 
 
-sys.stdout.write("int hehe[%d] = " % len(var))
-#print enc
-count = 0
-sys.stdout.write("{")
-for val in enc:
-    sys.stdout.write(hex(ord(val)))
-    if count != len(enc)-1:
-        sys.stdout.write(",")
-    count = count + 1
+    sys.stdout.write("int hehe[%d] = " % len(var))
+    #print enc
+    count = 0
+    sys.stdout.write("{")
+    for val in enc:
+        sys.stdout.write(hex(ord(val)))
+        if count != len(enc)-1:
+            sys.stdout.write(",")
+        count = count + 1
 
-sys.stdout.write("};")
-print
-#print pkey
+    sys.stdout.write("};")
+    print
+    #print pkey
 
-sys.stdout.write("int haha[%d] = " % len(var))
-count = 0
-sys.stdout.write("{")
-for val in pkey:
-    sys.stdout.write(hex(ord(val)))
-    if count != len(enc)-1:
-        sys.stdout.write(",")
-    count = count + 1
+    sys.stdout.write("int haha[%d] = " % len(var))
+    count = 0
+    sys.stdout.write("{")
+    for val in pkey:
+        sys.stdout.write(hex(ord(val)))
+        if count != len(enc)-1:
+            sys.stdout.write(",")
+        count = count + 1
 
-sys.stdout.write("};")
-print
-#print [ chr(ord(a) ^ ord(b)) for (a,b) in zip(enc, pkey) ]
+    sys.stdout.write("};")
+    print
+    print "Input a string to be XORed >> "
+    string = raw_input()
